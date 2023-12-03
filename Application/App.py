35f69bdb5b940e71adf5bb439cc3df2762f6cdbe
DeonXavier <deonxavier2019@gmail.com>
2023-12-03 15:37:40 +0530

    Application first 1

--- Application/App.py
+++ Application/App.py
@@ -1,2 +1,12 @@
 import API
-from flask import Flask, rende
+from flask import Flask, render_template, request, flash, redirect
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    durl='#'
+    return render_template('index.html',durl=durl)
+
+if __name__ == '__main__':
+    app.run(debug=True)
